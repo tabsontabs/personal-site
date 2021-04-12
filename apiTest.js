198b6bbf8f71db88361366524a18e3d3a262c5b2
tabsontabs <tabithapoeze@gmail.com>
2021-04-12 13:49:34 -0400

    api testing

---
+++ apiTest.js
@@ -0,0 +1,33 @@
+const testJJ = {};
+
+testJJ.url = 'https://api.controlport.co.uk/api/1/order';
+
+testJJ.testAPI = () => {
+    $.ajax({
+        url: `${testJJ.url}`,
+        method: 'POST',
+        dataType: 'json',
+        data: {
+            api_key: '626ba2f371ad95f267de7a02c8cc6601'
+        }
+    }).then((response) => {
+        if (response.success = true ) {
+            console.log('true');
+        }
+
+        if (response.success = false) {
+            console.log('false');
+        }
+
+        console.log(response);
+    });
+}
+
+testJJ.init = () => {
+    testJJ.testAPI();
+}
+
+
+$(function () {
+    testJJ.init();
+});
