a57e7193af3a699b7872db87422d5fa5fa208fc9
tabsontabs <tabithapoeze@gmail.com>
2021-04-12 14:04:41 -0400

    api testing 4

--- apiTest.js
+++ apiTest.js
@@ -1,14 +1,22 @@
 const testJJ = {};
 
 testJJ.url = 'https://api.controlport.co.uk/api/1/order';
+testJJ.fullAPI = '626ba2f371ad95f267de7a02c8cc6601'
+testJJ.halfAPI = '626ba2f371ad95f2'
+testJJ.timestamp = Math.round((new Date()).getTime() / 1000);
+
+console.log(testJJ.timestamp);
 
 testJJ.testAPI = () => {
+
     $.ajax({
         url: `${testJJ.url}`,
         method: 'POST',
         dataType: 'json',
         data: {
-            "api_key": "626ba2f371ad95f267de7a02c8cc6601"
+            half_api_key: `${testJJ.halfAPI}`,
+            message_timestamp: 1618186394,
+            security_hash: '75abfe199348058913650c2603d4f5e1'
         }
     }).then((response) => {
         if (response.success = true ) {
